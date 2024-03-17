80e262bf973dfacef372c06e49f8ed4310dfa4e9
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-17 20:21:17 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import test from "node:test";
+import "./globals.css"
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-2xl">Hello world :) </h1>
+      <p className="text-6xl">test</p>
+    </main>
+  );
+}
